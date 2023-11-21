615840ae82cb68c15fb32187fb78cbac683a0fde
fardan-nca <fardan.syed@news.com.au>
2023-11-21 19:25:41 +0530

    formated document

--- components/AddTodo.vue
+++ components/AddTodo.vue
@@ -1,90 +1,95 @@
 <template>
-<div>
-    <form  class="todo-form" @submit.prevent="todoData">
-       <input type="text" v-model="newTodo" placeholder="What you will to do today" class="todo-input">
-       <button type="submit" class="add-button">Add Todo</button>
+  <div>
+    <form class="todo-form" @submit.prevent="todoData">
+      <input
+        type="text"
+        v-model="newTodo"
+        placeholder="What you will to do today"
+        class="todo-input"
+      />
+      <button type="submit" class="add-button">Add Todo</button>
     </form>
-    <TodoList :todoData = todos @removeItem="removeList($event)"></TodoList>
-</div>
+    <TodoList :todoData="todos" @removeItem="removeList($event)"></TodoList>
+  </div>
 </template>
 <script>
-import TodoList from './TodoLIst.vue';
+import TodoList from "./TodoLIst.vue";
 export default {
-    name:'AddTodo',
-    data() {
-        return {
-            newTodo: '',
-            todos: []
-        }
+  name: "AddTodo",
+  data() {
+    return {
+      newTodo: "",
+      todos: [],
+    };
+  },
+  components: {
+    TodoList,
+  },
+  mounted() {
+    const cacheData = JSON.parse(localStorage.getItem("addedTodos"));
+    if (cacheData && cacheData.length > 0) {
+      this.todos = cacheData;
+    }
+  },
+  watch: {
+    todos: {
+      handler(val) {
+        localStorage.setItem("addedTodos", JSON.stringify(val));
+      },
+      deep: true,
     },
-    components:{
-        TodoList
-    },
-    mounted() {
-      const cacheData = JSON.parse(localStorage.getItem('addedTodos'));
-      if (cacheData && cacheData.length > 0) {
-        this.todos = cacheData;
+  },
+  methods: {
+    todoData() {
+      // console.log(this.newTodo);
+      if (this.newTodo) {
+        this.todos.push(this.newTodo?.trim());
+        this.newTodo = "";
       }
     },
-    watch: {
-        todos:{
-            handler(val){
-                localStorage.setItem('addedTodos', JSON.stringify(val));
-            },
-            deep:true,
-        }
+    removeList(index) {
+      this.todos.splice(index, 1);
+      // console.log(index)
     },
-    methods: {
-        todoData() {
-            // console.log(this.newTodo);
-            if(this.newTodo) {
-                this.todos.push(this.newTodo?.trim());
-                this.newTodo = '';
-            }
-        },
-        removeList(index) {
-            this.todos.splice(index,1)
-            // console.log(index)
-        }
-    }
-}
+  },
+};
 </script>
 <style>
 .todo-form {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap:10px
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
 }
 .todo-input {
-    width: 300px;
-    height: 50px;
+  width: 300px;
+  height: 50px;
 }
 .todo-input::placeholder {
-    text-align: center;
+  text-align: center;
 }
 .todo-list {
-    display: flex;
-    list-style: none;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    gap: 30px;
-    margin-top:1rem;
+  display: flex;
+  list-style: none;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 30px;
+  margin-top: 1rem;
 }
 .todo-list > li {
-  font-size: 1rem; 
+  font-size: 1rem;
   border: 1px solid;
   padding: 10px;
   width: 50vh;
   text-align: center;
 }
 .add-button {
-    width: 100px;
-    height: 50px;
-    background-color: #2196f3;
-    color: whitesmoke;
-    cursor: pointer;
-    font-weight: bold;
+  width: 100px;
+  height: 50px;
+  background-color: #2196f3;
+  color: whitesmoke;
+  cursor: pointer;
+  font-weight: bold;
 }
 </style>
